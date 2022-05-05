ae35c16930e406d3f0588f966a6d2b2dd35dc41a
Marahqbj <92785783+Marahqbj@users.noreply.github.com>
2022-05-05 19:40:47 +0300

    Add files via upload

---
+++ src/dms/Dms.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dms;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Marah qubbbaj
+ */
+public class Dms extends Application {
+      public void start(Stage primaryStage) throws Exception{
+        
+  
+     
+    Parent root = FXMLLoader.load(getClass().getResource(".fxml"));
+  primaryStage.setTitle(" DMS ");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
